9254583f2e12bcbd4e2b910803d0492f4cc7fd80
Anton <anton.goetzee-barral@airelogic.com>
2022-08-15 10:43:06 +0100

    expanding mb api

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from typing import List
+from collections import Counter
+
 import requests
 import musicbrainzngs
+
+from src.lyric_store import RAW_LYRICS
 
 musicbrainzngs.set_useragent("lyrics_analytics", "0.1", "antongb09@gmail.com")
 
@@ -22,9 +27,43 @@ def match_artist_id(search_artist: str, artist_list: dict) -> list:
     return artist_ids  
 
 
-print(get_id_by_artist("Justin Bieber"))
-
-
-def get_lyrics(artist: str, title: str):
+def get_lyrics(artist: str, title: str) -> str:
     url = f"https://api.lyrics.ovh/v1/{artist}/{title}"
+    response = requests.get(url)
+    return response.json()["lyrics"]
+
+
+def get_works(id: str): ...
+
+
+def check_artist_id(artist_name: str, artist_id: str) -> bool:
+    artist_definition = musicbrainzngs.get_artist_by_id(artist_id)
+    artist_name_musicbrainz = artist_def["artist"]["name"].lower()
+    return artist_name.lower() == artist_name_musicbrainz
+
+
+def lytic_occurrence(lyrics: list) -> dict: # could use pandas
+    occurrences = Counter(tuple(lyrics))
+    return dict(occurrences)
+
+def clean_lyrics(lyrics: str, replacing_chars: List[str]) -> str:
+    lyrics = lyrics.lower()
+    for char in chars_to_remove:
+        lyrics = lyrics.replace(char, " ")
     
+    return " ".join(lyrics.split())
+
+
+artist = "justin bieber"
+work = "baby"
+artist_id = get_id_by_artist(artist)
+lyrics = get_lyrics(artist, work)
+
+
+
+chars_to_remove = ["\n", "\r", "\t" ,"(", ")", "[", "]", ":", ",", "."]
+clean_lyrics = clean_lyrics(RAW_LYRICS, chars_to_remove)
+list_lyrics = clean_lyrics.split(" ")
+
+
+
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/agb/Documents/lyrics_analytics/venv"
+setenv VIRTUAL_ENV "/home/agb/personal/lyrics_analytics/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
