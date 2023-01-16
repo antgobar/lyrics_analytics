4f6fd156fa5014ded4397f4d81a90771032802f0
Anton <antongb09@gmail.com>
2023-01-16 20:49:00 +0000

    update flask flow

--- tests/api/test_search.py
+++ tests/api/test_search.py
@@ -5,35 +5,57 @@ import pytest
 from lyrics_analytics.api import create_app
 
 
-class TestIndex:
-    def test_index_get(self):
-        app = create_app()
-        with app.test_client() as c:
-            response = c.get("/")
-            assert response.status_code == 200
-            assert b"""<label for="name">Artist Name</label>""" in response.data
-            assert b"""<input name="name" id="name"required>""" in response.data
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
 
-    @pytest.mark.parametrize("data", [
-        "metallica", ""
-    ])
-    def test_index_post(self, data):
-        app = create_app()
-        client = app.test_client()
-        response = client.post("/", data={"name": data})
-        assert response.status_code == 302
 
-    @patch("lyrics_analytics.api.search.find_artists")
-    def test_index_post(self, mock_find_artists):
-        app = create_app()
-        mock_find_artists.return_value = [
-            {"id": 1, "name": "metallica"},
-            {"id": 2, "name": "metallica 2"}
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+def test_index_get(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"""<label for="name">Artist Name</label>""" in response.data
+    assert b"""<input name="name" id="name"required>""" in response.data
+
+
+@pytest.mark.parametrize("data", [
+    "metallica", ""
+])
+def test_index_post(client, data):
+    response = client.post("/", data={"name": data})
+    assert response.status_code == 302
+
+
+@patch("lyrics_analytics.api.search.genius_service.find_artists")
+@pytest.mark.parametrize(("artist", "find_artist_return", "expected"), [
+    (
+        "metallica",
+        [{"id": 1, "name": "metallica"}, {"id": 2, "name": "metallica 2"}],
+        [
+            b"""<a class="action" href="/artist?id=1"><h1>metallica</h1></a>""",
+            b"""<a class="action" href="/artist?id=2"><h1>metallica 2</h1></a>"""
         ]
-        with app.test_client() as c:
-            response = c.post("/", data={"name": "metallica"})
-            assert response.status_code == 200
-            assert b"""<a class="action" href="/artist?artist_id=1"><h1>metallica</h1></a>""" in response.data
-            assert b"""<a class="action" href="/artist?artist_id=2"><h1>metallica 2</h1></a>""" in response.data
+    ),
+    (
+        None,
+        [],
+        [b"""<label for="name">Artist Name</label>"""]
+    )
+])
+def test_artists(mock_find_artists, client, artist, find_artist_return, expected):
+    mock_find_artists.return_value = find_artist_return
+    response = client.get(f"/artists?name={artist}", follow_redirects=True)
+    assert response.status_code == 200
+    for html in expected:
+        assert html in response.data
 
-        mock_find_artists.assert_called_with("metallica")
+    if artist is not None:
+        mock_find_artists.assert_called_with(artist)
