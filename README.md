a0a788b710d05cc963e442e774da68cd2e23e285
Anton <antongb09@gmail.com>
2023-01-16 22:03:38 +0000

    reading list

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lyrics Analytics
+
+reading list
+[article](https://towardsdatascience.com/how-to-set-up-a-production-grade-flask-application-using-application-factory-pattern-and-celery-90281349fb7a)
+https://github.com/angeuwase/production-flask-app-setup/tree/main/app/auth
--- lyrics_analytics/tasks/tasks.py
+++ lyrics_analytics/tasks/tasks.py
@@ -1,7 +1,15 @@
+import os
+
 from celery import Celery
 
 
 def make_celery(app):
+    app.config.update(
+        CELERY_CONFIG={
+            "broker_url": os.environ.get("CELERY_BROKER_URL", "amqp://localhost:5672"),
+            "result_backend": os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379"),
+        }
+    )
     celery = Celery(app.import_name)
     celery.conf.update(app.config["CELERY_CONFIG"])
 
