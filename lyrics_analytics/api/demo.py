fb571a639368f3597afee7d0f60a438188b82794
Anton <antongb09@gmail.com>
2023-02-09 22:40:03 +0000

    demo with spinner button

--- lyrics_analytics/api/demo.py
+++ lyrics_analytics/api/demo.py
@@ -12,13 +12,19 @@ def index():
 
     name = request.form["artist-name"]
 
-    flash(f"Searching {name}")
-
     found = [
         {"name": name, "id": 1},
         {"name": "Metallica", "id": 2},
         {"name": "Taylor Swift", "id": 3}
     ]
-
+    import time
+    time.sleep(3)
     return render_template("demo/artists-found.html", artists=found)
 
+
+@bp.route("/artist")
+def artist():
+    artist_id = request.args.get("id")
+    name = request.args.get("name")
+
+    return render_template("demo/index.html")
