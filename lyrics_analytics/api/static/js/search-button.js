f32b24ee0133764302af1a198c052df65e59a574
Anton <antongb09@gmail.com>
2023-02-10 19:41:40 +0000

    update index

--- lyrics_analytics/api/static/js/search-button.js
+++ lyrics_analytics/api/static/js/search-button.js
@@ -1,5 +1,5 @@
 function submitForm() {
-var btn = document.getElementById('search-button');
-btn.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
-btn.setAttribute('disabled', 'disabled');
+    var btn = document.getElementById('search-button');
+    btn.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Searching ...';
+    btn.setAttribute('disabled', 'disabled');
 }
